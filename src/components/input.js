ac84c1d0b1a34d29e9210fb06d3ea7348b5eb0f2
Kvirike kemularia <kvirikekemularia@gmail.com>
2024-03-06 19:23:28 +0400

    useState for the input value
    for the first run, getting input value and storing it in the state, then using map to render each item from the array as a list

--- src/components/input.js
+++ src/components/input.js
@@ -1,26 +1,49 @@
 import { Button } from 'react-bootstrap';
 import remove from '../media/remove.png'
 import done from '../media/done.png'
+import { useEffect, useState} from 'react';
+
 
 function Input() {
+    const[inpVal, setInptVal] = useState('')
+    const[currentVal, updtVal] = useState([])
+
+
+    const handleChange = (event) => {
+      setInptVal(event.target.value);
+    };
+
+    function submitChange(event) {
+      event.preventDefault();
+      
+      if (inpVal.trim() === '') {
+        return;
+      }
+
+      updtVal([...currentVal, inpVal]);
+      setInptVal('');
+      
+    }
+
     return(
         <div className='bodyDiv'>
-          <form className='form'>
+          <form className='form' onSubmit={submitChange}>
             <input
               className='inp'
+              value={inpVal}
+              onChange={handleChange}
             ></input>
-            <Button className='d-flex justify-content-center align-items-center addBtn'>Add</Button>
+            <Button className='d-flex justify-content-center align-items-center addBtn' type="submit">Add</Button>
           </form>
           <ul>
-            <li>
-              <div className='task-content'>
-                <span className='task-name'>Task one</span>
-                <div>
-                  <img className='taskBtns' src={remove} alt='Delete'></img>
-                  <img className='taskBtns' src={done} alt='Add'></img>
-                </div>
-              </div>
-            </li>
+              {currentVal.map((elem, index) => {
+                return <li key={index}>
+                          <span className='task-name'>{elem}</span>
+                          <img className='taskBtns' src={remove} alt='Delete'></img>
+                          <img className='taskBtns' src={done} alt='Add'></img>
+                        </li>
+                })
+              } 
           </ul>
         </div>
     )
